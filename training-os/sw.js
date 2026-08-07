bd0b0e413afb6421012071077ec5b2b3967a55f7
ljw6430 <ljw6430@gmail.com>
2026-08-07 18:41:35 +0900

    Refresh PWA cache for four-way split redesign

--- training-os/sw.js
+++ training-os/sw.js
@@ -1,10 +1,12 @@
-const C='training-os-black-live-v1';
+const C='training-os-black-live-v2';
 const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const clone=resp.clone();caches.open(C).then(c=>c.put(e.request,clone));return resp;
-  }).catch(()=>caches.match('./index.html'))));
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(resp=>{const clone=resp.clone();caches.open(C).then(c=>c.put('./index.html',clone));return resp}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const clone=resp.clone();caches.open(C).then(c=>c.put(e.request,clone));return resp})));
 });
